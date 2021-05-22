9377df1f21ae69794c0990f77d9d8d9a5024fed5
Aniruddha Maru <maru@standard.ai>
2021-05-21 20:39:53 -0700

    remove copyright notice

--- consumer.go
+++ consumer.go
@@ -1,7 +1,4 @@
 /*
- * Copyright 2018, Automatic Inc.
- * All rights reserved.
- *
  * Author: Michael Ngo
  */
 
--- models_test.go
+++ models_test.go
@@ -1,7 +1,4 @@
 /*
- * Copyright 2017, Automatic Inc.
- * All rights reserved.
- *
  * Author: Michael Ngo
  */
 
