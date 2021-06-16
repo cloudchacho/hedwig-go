05ce14f45c4f464139929802faf14d53380c28b9
Aniruddha Maru <maru@standard.ai>
2021-06-16 10:14:51 -0700

    install right thing

--- run-tests.sh
+++ run-tests.sh
@@ -8,7 +8,7 @@ cleanup() {
 }
 
 setup_pubsub_emulator() {
-  gcloud components install pubsub-emulator
+  gcloud components install beta pubsub-emulator
   gcloud beta emulators pubsub start --project emulator-project > /dev/null 2>&1 &
   emulator_pid=$!
   trap cleanup EXIT
