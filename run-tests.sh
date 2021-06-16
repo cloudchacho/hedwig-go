887846bc004350f6ed4f14596b56a85a4e866f3e
Aniruddha Maru <maru@standard.ai>
2021-06-16 10:04:58 -0700

    debug

--- run-tests.sh
+++ run-tests.sh
@@ -8,7 +8,7 @@ cleanup() {
 }
 
 setup_pubsub_emulator() {
-  gcloud components install beta
+  gcloud components install pubsub-emulator
   gcloud beta emulators pubsub start --project emulator-project > /dev/null 2>&1 &
   emulator_pid=$!
   trap cleanup EXIT
